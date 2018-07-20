acd3b0b792c8cc168418d070f68070d5eff4484e
Aleksei Voronov <despawn@gmail.com>
2018-07-20 18:28:47 +0300

    Recalculate spreadsheet fully in proper order on changes

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ use formula::Formula;
 use graph::DirectedGraph;
 use reference::Reference;
 use sheet::{Cell, Sheet};
-use value::InvalidValue;
+use value::{MaybeValue, InvalidValue};
 
 #[derive(Debug, Default)]
 struct Spreadsheet {
@@ -38,17 +38,13 @@ impl Spreadsheet {
         self.cells.get(&reference.into())
     }
 
-    pub fn get_mut<R: Into<Reference>>(&mut self, reference: R) -> Option<&mut Cell> {
-        self.cells.get_mut(&reference.into())
-    }
-
-    pub fn get_or_empty_mut<R: Into<Reference>>(&mut self, reference: R) -> &mut Cell {
+    pub fn get_guaranteed<R: Into<Reference>>(&mut self, reference: R) -> &mut Cell {
         self.cells
             .entry(reference.into())
             .or_insert_with(Cell::default)
     }
 
-    pub fn set<R, T>(&mut self, reference: R, input: T)
+    pub fn enter<R, T>(&mut self, reference: R, input: T)
     where
         R: Into<Reference>,
         T: ToString,
@@ -56,46 +52,64 @@ impl Spreadsheet {
         let reference = reference.into();
         let input = input.to_string();
 
-        let (value, formula) = if input.starts_with('=') {
+        if input.starts_with('=') {
             match Formula::from_str(&input) {
-                Ok(formula) => (evaluate(&formula.expr, &self.cells), Some(formula)),
-                Err(_) => (Err(InvalidValue), None),
+                Ok(formula) => {
+                    self.dependencies.set_incoming_edges(reference, formula.find_references());
+                    let cell = self.get_guaranteed(reference);
+                    cell.input = input;
+                    cell.formula = Some(formula);
+                },
+                Err(_) => {
+                    let cell = self.get_guaranteed(reference);
+                    cell.value = Err(InvalidValue);
+                    cell.input = input;
+                    cell.formula = None;
+                }
             }
         } else {
-            (input.parse(), None)
-        };
-
-        if let Some(ref formula) = formula {
-            self.dependencies
-                .set_incoming_edges(reference, formula.find_references());
+            let cell = self.get_guaranteed(reference);
+            cell.value = input.parse();
+            cell.input = input;
+            cell.formula = None;
         }
 
-        let cell = self.get_or_empty_mut(reference);
-        cell.input = input;
-        cell.value = value;
-        cell.formula = formula;
+        self.recalculate();
+    }
+
+    fn recalculate(&mut self) {
+        let order = self.dependencies.to_topological_sort().unwrap();
+        for reference in order.iter().cloned() {
+            if let Some(value) = self.calculate_cell(reference) {
+                self.get_guaranteed(reference).value = value
+            }
+        }
+    }
+
+    fn calculate_cell(&self, reference: Reference) -> Option<MaybeValue> {
+        self.get(reference).and_then(|cell| cell.formula.as_ref()).map(|formula| evaluate(&formula.expr, &self.cells))
     }
 }
 
 fn main() {
     let mut spreadsheet = Spreadsheet::new();
-    spreadsheet.set("A1", 10);
-    spreadsheet.set("A2", 20);
-    spreadsheet.set("A3", 30);
+    spreadsheet.enter("A1", 10);
+    spreadsheet.enter("A2", 20);
+    spreadsheet.enter("A3", 30);
 
-    spreadsheet.set("B1", "=20");
-    spreadsheet.set("B2", "=30+50");
+    spreadsheet.enter("B1", "=20");
+    spreadsheet.enter("B2", "=30+50");
 
-    spreadsheet.set("C1", "=A1*2");
-    spreadsheet.set("C2", "=A1+A2+A3");
-    spreadsheet.set("C3", "=A4 + 20");
+    spreadsheet.enter("C1", "=A1*2");
+    spreadsheet.enter("C2", "=A1+A2+A3");
+    spreadsheet.enter("C3", "=A4 + 20");
 
     for (reference, cell) in &spreadsheet.cells {
         println!("{:?} :: {:?}", reference, cell);
     }
 
     println!(
-        "Sorted topologically: {:?}",
+        "Order of evaluation: {:?}",
         spreadsheet.dependencies.to_topological_sort()
     );
 }
